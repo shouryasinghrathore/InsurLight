daba30c851c2e4788341398e852fa4329797429a
Shourya Singh Rathore <shourya26rathore@gmail.com>
2025-08-10 02:49:39 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Enable CORS
+    app.use(cors({
+      origin: '*', 
+    }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
